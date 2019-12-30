5d1e99c638180d8ad34146ffffd3d3c6c1c07047
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2019-12-30 11:52:08 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,14 +13,12 @@ client.on('message', message => {
   if (message.channel.id === '458527068188180501') {
     if (message.content) {
       let emoji = client.emojis.find(x => x.name === "dice2")
-      let emoji2 = client.emojis.find(x => x.name === "white_check_mark")
       message.react(emoji)
-      message.react(emoji2)
+      message.react('✅')
     } else {
       return;
     }
   }
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
