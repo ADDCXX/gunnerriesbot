1545a045533190d8372f74610ff222eef077a749
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2019-12-30 12:19:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-  
-  function randomStatus() {
-        let status = ['Under Construction', 'Support Developer by Ray'];
-        let rstatus = Math.floor(Math.random() * status.length);
-        client.user.setActivity(status[rstatus], {type: 0}); // bisa diganti: PLAYING LISTENING WATCHING STREAMING
-
-    }; setInterval(randomStatus, 60000) // 60000 = 60 detik (ms)
-});
-
 client.on('message', message => {
   if (message.content === 'Link') {
     message.reply('https://discord.gg/dzXypuu');
