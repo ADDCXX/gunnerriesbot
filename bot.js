fd937130f7f5a866914c34465eaa7f2fa2293302
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2019-12-30 12:01:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+  
+  function randomStatus() {
+        let status = ['Under Constrution', 'support developer by Ray'];
+        let rstatus = Math.floor(Math.random() * status.length);
+        client.user.setActivity(status[rstatus], {type: 'PLAYING'}); // bisa diganti: PLAYING LISTENING WATCHING STREAMING
+
+    }; setInterval(randomStatus, 60000) // 60000 = 60 detik (ms)
 });
 
 client.on('message', message => {
@@ -12,9 +19,9 @@ client.on('message', message => {
   
   if (message.channel.id === '458527068188180501') {
     if (message.content) {
-      let emoji = client.emojis.find(x => x.name === "dice2")
+      let emoji = client.emojis.find(x => x.name === "dice2") // khusus server emoji
       message.react(emoji)
-      message.react('✅')
+      message.react('✅') // khusus unicode (https://emojipedia.org/)
     } else {
       return;
     }
