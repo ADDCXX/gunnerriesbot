0b16468978b43c3b79477dbea5a1520f2a07e304
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2019-12-30 14:33:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('I am ready!');
   
   function randomStatus() {
-        let status = ['Patroling around the City 🚔', 'I was just an ordinary robot who was ordered to make other people happy | Spesial thanks to Ray'];
+        let status = ['Patroling around the City 🚔 24/7', 'I was just an ordinary robot who was ordered to make other people happy | Spesial thanks to Ray'];
         let rstatus = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[rstatus], {type: 0}); // bisa diganti: PLAYING LISTENING WATCHING STREAMING
 
