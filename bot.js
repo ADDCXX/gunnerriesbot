fdfdc4f73b9f480bc7451f6bc6c492ef9b672ae3
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2020-01-03 00:44:09 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,8 @@ client.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.find(channel => channel.id === "276225910464708610");
     if(!channel) return;
 
-    channel.send(`**Welcome aboard!** ${member}, Please register your biodata information here! #📝introductions`)
+    channel.send(`**Welcome Aboard !** ${member}, 
+Please register your biodata information here! "#📝introductions "`)
 
   });
 
