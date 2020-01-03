6c792b422b62f10cf60611d48ff5990da8f36493
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2020-01-03 10:33:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 
   function randomStatus() {
-        let status = ['Patroling around the City 🚔 24/7', 'Don't forget to understand the rules and conditions of this server !'];
+        let status = ['Patroling around the City 🚔 24/7', 'Dont forget to Obey the rules and conditions of this server !'];
         let rstatus = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[rstatus], {type: 0}); // bisa diganti: PLAYING LISTENING WATCHING STREAMING
 
