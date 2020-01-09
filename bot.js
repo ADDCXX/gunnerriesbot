15e631c2c1ed72ccee7d195ae94cca5d741a1dff
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2020-01-09 18:38:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', message => {
     if (message.content) {
       let emoji = client.emojis.find(x => x.name === "dice2") // khusus server emoji
       message.react(emoji)
-      message.react('âœ…') // khusus unicode (https://emojipedia.org/)
+      message.react('✅') // khusus unicode (https://emojipedia.org/)
     } else {
       return;
     }
