4c2459f80f0ed4046cf545fc6b758ae06caebb86
Gunnerries <50061829+ADDCXX@users.noreply.github.com>
2022-02-05 09:50:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 
   function randomStatus() {
-        let status = ['Patroling around the Gunnerries Project 🚔 24/7', 'Dont forget to obey the rules and conditions of this Spaceship !'];
+        let status = ['Patroling around the District Visual 🚔 24/7', 'Dont forget to obey the rules and conditions of this Server !'];
         let rstatus = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[rstatus], {type: 0}); // bisa diganti: PLAYING LISTENING WATCHING STREAMING
 
